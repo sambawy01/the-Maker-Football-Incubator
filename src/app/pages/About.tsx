6878b6feceded85063c8d2f1c57c9bf9a05fee64
sambawy01 <sambawy@gmail.com>
2026-02-17 20:55:08 +0200

    Update Yosra photo

--- src/app/pages/About.tsx
+++ src/app/pages/About.tsx
@@ -132,7 +132,7 @@ const Timeline = () => {
 const Team = () => {
   const leaders = [
     { name: "Ahmed Hossam ‘Mido’", role: "Founder & Director • Sporting Director, Enosis Paralimni", img: founderImg },
-    { name: "Yosra Elleithy", role: "People & Culture Director • Podcast Co-Host", img: "https://images.unsplash.com/photo-1655249493799-9cee4fe983bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwcm9mZXNzaW9uYWwlMjBidXNpbmVzcyUyMHdvbWFuJTIwcG9ydHJhaXQlMjBoZWFkc2hvdHxlbnwxfHx8fDE3NzEyOTg3NzR8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral" },
+    { name: "Yosra Elleithy", role: "People & Culture Director • Podcast Co-Host", img: "https://i.ibb.co/Q76W5Tfx/Screenshot-2026-02-17-at-8-48-17-PM.png" },
     { name: "Tamer Wasfy", role: "Managing Director", img: "https://images.unsplash.com/photo-1723537742563-15c3d351dbf2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwcm9mZXNzaW9uYWwlMjBidXNpbmVzcyUyMG1hbiUyMHBvcnRyYWl0JTIwaGVhZHNob3R8ZW58MXx8fHwxNzcxMjk4Nzc0fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral" },
   ];
 
